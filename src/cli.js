0379970f7f082faa17de1aa38d17c89b40fb04c1
Dmitriy Averkov <averkov.d.v@bk.ru>
2023-03-15 18:06:56 +0300

    Установите следующие пакеты: eslint, eslint-config-airbnb-base, eslint-plugin-import     Настройте правильно .eslintrc.yml. Документация: https://eslint.org/docs/user-guide/configuring. Сверяйтесь с nodejs-package     Создайте задачу make lint, которая должна запускать npx eslint .     Подключите eslint к своему редактору, убедитесь что все работает. Скорее всего, понадобится поставить eslint глобально     Исправьте все ошибки, которые выдает линтер. Отключите правила no-console (оно не актуально для nodejs разработки) и import/extensions (чтобы получить возможность указывать при импорте файлы с расширением)

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export const greetingByName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return console.log('Hello, ' + userName + '!');
-};
+
+export default function greetingByName() {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
+}
